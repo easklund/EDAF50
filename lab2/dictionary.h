e74f18c96a17d0d227b54ff5adb89dc0b6d7b27a
easklund <dat14eas@student.lu.se>
2018-02-06 13:43:19 +0100

    dictionary.h fixad

--- lab2/dictionary.h
+++ lab2/dictionary.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <unordered_set>
+#include "word.h"
 
 class Dictionary {
 public:
@@ -12,6 +13,7 @@ public:
 	std::vector<std::string> get_suggestions(const std::string& word) const;
 private:
 	std::unordered_set<std::string> words;
+	std::vector<Word> words[25];
 };
 
 #endif
