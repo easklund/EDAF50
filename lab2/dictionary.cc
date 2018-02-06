27043ce7196c1bf2ca92b8651e13635c104c08a5
Tobias94Olsson <dat14tol@student.lu.se>
2018-02-06 11:51:14 +0100

    Finished task A2

--- lab2/dictionary.cc
+++ lab2/dictionary.cc
@@ -3,17 +3,33 @@
 #include <fstream>
 #include <iostream>
 #include <algorithm>
+#include <unordered_set>
 #include "word.h"
 #include "dictionary.h"
 
 using std::string;
 using std::vector;
+using std::unordered_set;
+using std::ifstream;
+using std::cout;
+
+unordered_set<string> words;
 
 Dictionary::Dictionary() {
+	words = unordered_set<string>();
+	ifstream infile("words.txt");
+	string wordline;
+	for(string line; getline(infile, line);){
+		wordline = line.substr(0, line.find(" "));
+		words.insert(wordline);
+	}
 }
 
 bool Dictionary::contains(const string& word) const {
-	return true;
+	if(words.count(word) > 0){
+		return true;
+	}
+	return false;
 }
 
 vector<string> Dictionary::get_suggestions(const string& word) const {
