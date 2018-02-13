2a1abd498ca1475f845ad14480e09a9eecdb821f
Tobias94Olsson <dat14tol@student.lu.se>
2018-02-13 11:52:06 +0100

    test works, but needs refactor

--- lab3/test_tag_remover.cc
+++ lab3/test_tag_remover.cc
@@ -9,14 +9,13 @@ using std::cin;
 
 void testTagRemove() {
   string test = "en Tag: <första>funkar en till:<andra>&lt&nbsp&amp&gt";
-  string right = "en Tag: första funkar en till: andra";
-  //cin >> test;
+  string right = "en Tag: funkar en till:< &>";
   TagRemover tr(test);
   string testRemoved = tr.getText();
   if (testRemoved == right)
-    cout << "TagRemover worked" << endl;
+    cout << "<<TagRemover worked>>" << endl;
   else
-    cout << "TagRemover failed" << endl;
+    cout << "<<TagRemover failed>>" << endl;
   cout << testRemoved << endl;
 }
 
