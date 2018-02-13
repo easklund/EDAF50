92632296c63af6242da7dfd218a08089dcb60a44
Tobias94Olsson <dat14tol@student.lu.se>
2018-02-13 21:07:17 +0100

    algorithm for finding primes correct, still missing correct test and cleanup

--- lab3/prime.cc
+++ lab3/prime.cc
@@ -1,30 +1,39 @@
 #include "prime.h"
 #include <string>
 #include <iostream>
+#include <sstream>
 
 using std::endl;
 using std::cout;
 using std::string;
+using std::stringstream;
 
 string Prime::primesUnderN(const int n){
 	string primes = "";
-	string primeLetters = string(n-1, 'P');
+	string primeLetters = string(n+1, 'P');
 	primeLetters[0] = 'C';
 	primeLetters[1] = 'C';
 	for(int i = 2; i <= n; i++){
 		primeLetters[i] = 'P';
 	}
-	for(int p = 2; p < n; p++){
+	for(int p = 2; p*p< n; p ++){
 		if(primeLetters[p] == 'P'){
-			for(int i = p*2; i < n; i += p){
+			for(int i = p*2; i <= n; i += p){
 				primeLetters[i] = 'C';
 			}
+		}
+	}
+	stringstream ss;
+	for(int p = 2; p<=n; p++){
+		if(primeLetters[p] == 'P'){
+			ss << p << " ";		
 		}
 	}
 
 	cout << primeLetters << endl;
 
-
+	cout << ss.str() << endl;
+	primes = ss.str();
 	return primes;
 }
 
